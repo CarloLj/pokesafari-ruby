6cadb01c4d78f43a2a4c0b19a96a0a8f26811b55
Ruy Guzmán Camacho <ruy.guzman2002@gmail.com>
2023-04-23 23:44:18 -0600

    code coverage

--- spec/controllers/pokemons_controller_spec.rb
+++ spec/controllers/pokemons_controller_spec.rb
@@ -1,18 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe PokemonsController, type: :controller do
-  
     describe "Post Create" do
         context 'save pokemon correctly' do
-            subject {post :create, params: {pokemon:{ pokemon_id: 1}}}
+            #@trainer = FactoryBot.create(:trainer)
+
+            subject { post :create, params: { trainer_id: 1, pokemon: { pokemon_id: "1" } } }
 
             it 'should add to trainer' do
-            
+                created_pokemon = Trainer.find(1).pokemons.find_by(pokemon_id: "1")
+                expect(created_pokemon).not_to be_nil
             end
 
             it 'should redirect to same trainer' do
-                
+                expect(response).to be_successful
             end
+
         end
     end
 end
---
+++ spec/factories/trainer.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+    factory :trainer do
+      name { "The name" }
+    end
+  end
