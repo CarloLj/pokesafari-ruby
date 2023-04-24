2a78425713cf2a1359436c157417b74a91c7eaae
Carlo <carlouxljlj@gmail.com>
2023-04-23 23:01:45 -0600

    [show.html] Updated Pokemon UI

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -1,12 +1,12 @@
 class PokemonsController < ApplicationController
-    def create
-        @trainer = Trainer.find(params[:trainer_id])
-        @pokemon = @trainer.pokemons.create(pokemon_params)
-        redirect_to trainer_path(@trainer)
+  def create
+      @trainer = Trainer.find(params[:trainer_id])
+      @pokemon = @trainer.pokemons.create(pokemon_params)
+      redirect_to trainer_path(@trainer)
+    end
+  
+    private
+      def pokemon_params
+        params.require(:pokemon).permit(:pokemon_id)
       end
-    
-      private
-        def pokemon_params
-          params.require(:pokemon).permit(:pokemon_id)
-        end
 end
