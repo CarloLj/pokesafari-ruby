803c7e22fd51580e3f5615ec0b26e45d8626eadf
Ricardo Glez Leal <53849239+RicardoGLeal@users.noreply.github.com>
2023-04-23 23:45:42 -0600

    Update trainer.rb

--- spec/factories/trainer.rb
+++ spec/factories/trainer.rb
@@ -1,8 +1,5 @@
 FactoryBot.define do
     factory :trainer do
-      first_name { 'John' }
-      last_name  { 'Doe' }
-      email { john@email_provider.com }
-      mobile_number { 7860945310 }
+        name { "Ricardo"}
     end
   end 
