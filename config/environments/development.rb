52df3d1a1533754201279f5d1a92b0659f038d9a
Carlo <carlouxljlj@gmail.com>
2023-05-25 21:35:46 -0600

    SIgn in and up gmail passwordupdate

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,13 +40,15 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { host: "localhost:3000" }
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.smtp_settings = {
     :address              => 'smtp.gmail.com',
     :port                 => 587,
     :domain               => 'gmail.com',
-    :user_name            => 'ruy.guzman.camacho@gmail.com',
-    :password             => ENV['MAILPASSWD'],
-    :authentication       => 'plain'
+    :user_name            => ENV["GMAIL_USERNAME"],
+    :password             => ENV["GMAIL_PASSWORD"],
+    :authentication       => 'plain',
+    :enable_starttls_auto => true,
   }
 
   config.action_mailer.perform_caching = false
